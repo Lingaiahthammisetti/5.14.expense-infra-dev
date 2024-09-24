50fe76650600ea3414b22c0961de6d5a9db34dab
lingaiah-ctu <sivalingapractise555@gmail.com>
2024-09-24 23:15:44 +0530

    expense infra dev msg

---
+++ 07.1-acm/variable.tf
@@ -0,0 +1,20 @@
+variable "project_name" {
+    default = "expense"
+}
+variable "environment" {
+  default =  "dev"
+}
+variable "common_tags" {
+  default =  {
+     Project = "expense"
+     Environment = "dev"
+     Terraform = true
+    # Component ="backend"
+  }
+}
+variable "zone_name" {
+  default = "lingaiah.online"
+  }
+  variable "zone_id" {
+    default = "Z02765181CEB7C51AA89M"
+  }
---
+++ 09-frontend/frontend.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+compoent=$1
+environment=$2
+dnf install ansible -y
+pip3.9 install botocore boto3
+ansible-pull -i localhost, -U https://github.com/linga-daws-78s/expense-ansible-roles-tf.git main.yaml -e component=$compoent -e env=$environment
