fae1d706c8974052e212c3f797c9f0b487a4eb89
lingaiah-ctu <sivalingapractise555@gmail.com>
2024-12-30 16:06:10 +0530

    ansible roles for tf infra dev

--- 07-backend/backend.sh
+++ 07-backend/backend.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-compoent=$1
+component=$1
 environment=$2
 dnf install ansible -y
 pip3.9 install botocore boto3
 ansible-pull -i localhost, -U https://github.com/Lingaiahthammisetti/4.10.2.expense-ansible-roles-tf-Infra-Dev.git main-tf.yaml -e component=$component -e env=$environment
-
