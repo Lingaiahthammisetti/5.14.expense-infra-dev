a5d31f730df1ecef5e9990e94c46df6d09620f23
lingaiah-ctu <sivalingapractise555@gmail.com>
2025-06-02 15:06:18 +0530

    expense infra dev

--- 07-backend/backend.sh
+++ 07-backend/backend.sh
@@ -3,4 +3,4 @@ component=$1
 environment=$2
 dnf install ansible -y
 pip3.9 install botocore boto3
-ansible-pull -i localhost, -U https://github.com/Lingaiahthammisetti/4.13.expense-ansible-roles-tf-Infra-Dev.git main-tf.yaml -e component=$component -e env=$environment
+ansible-pull -i localhost, -U https://github.com/Lingaiahthammisetti/4.13.expense-ansible-roles-tf-infra-dev.git main-tf.yaml -e component=$component -e env=$environment
